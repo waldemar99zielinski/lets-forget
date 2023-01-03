7a08e84dfda28abf93e00e6a165c884b2e0072a6
Waldemar Zieliński <waldemar99zielinski@gmail.com>
2023-01-04 00:09:38 +0100

    feat(frontend): add map component

---
+++ frontend/src/components/map/Map.tsx
@@ -0,0 +1,46 @@
+import 'leaflet/dist/leaflet.css';
+import './map.css';
+
+import * as L from 'leaflet';
+import { forwardRef, Ref } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet'
+
+import { MapMarker, MapMarkerProps } from './MapMarker';
+
+const warsawCoords: L.LatLngExpression = [52.22977, 21.01178];
+const warsawBounds: L.LatLngBoundsExpression = new L.LatLngBounds(
+    {lat: 52.2715188, lng: 21.120014},
+    {lat: 52.187930, lng: 20.903377}
+)
+const defaultZoom = 13;
+
+interface MapProps {
+    center?: L.LatLngExpression;
+    zoom?: number;
+    markers?: MapMarkerProps[];
+}
+
+export const Map = forwardRef((props: MapProps, ref: Ref<L.Map>) => {
+
+    const {center = warsawCoords, zoom = defaultZoom} = props;
+    
+    return <MapContainer center={center} zoom={13}
+        style={{
+            height: '100%',
+            width: '100%',
+        }}
+        ref={ref}
+        // TODO move it to backend dependent on the city :)
+        minZoom={13}
+        maxBounds={warsawBounds}
+    >
+        <TileLayer
+            attribution={`
+                <a href="https://github.com/PaulLeCam/react-leaflet" target="_blank">React Leaflet</a>
+                <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a>
+                <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap</a>`}
+            url='https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=ObuHobZwTIovUEDxxnZ0'
+        />
+        {props.markers && props.markers.map((markerProps) => <MapMarker {...markerProps} />)}
+  </MapContainer>;
+});
---
+++ frontend/src/components/map/MapMarker.tsx
@@ -0,0 +1,32 @@
+import * as L from 'leaflet';
+import ReactDOMServer from 'react-dom/server';
+import { Marker, Popup } from 'react-leaflet'
+
+import { Icon, IconProps } from 'src/components/icon';
+
+export interface MapMarkerProps {
+    latitude: number;
+    longitude: number;
+    icon?: IconProps;
+    popup?: JSX.Element;
+}
+
+export const MapMarker = (props: MapMarkerProps) => {
+
+    const icon = props.icon 
+        ? L.divIcon({
+            html: ReactDOMServer.renderToString(<Icon {...props.icon}/>) 
+        })
+        : undefined;
+
+    console.log(props.popup);
+
+    return <Marker 
+        position={{lat: props.latitude, lng: props.longitude}}
+        icon={icon}
+    >
+        {props.popup && <Popup>
+            {props.popup}
+        </Popup>}
+    </Marker>;
+}
