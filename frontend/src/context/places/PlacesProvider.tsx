72ca2d2e5a33ae7f221592ed7f292f4ab9293876
Waldemar Zieliński <waldemar99zielinski@gmail.com>
2023-01-04 00:10:25 +0100

    feat(frontend): add places context provider

---
+++ frontend/src/context/places/PlacesProvider.tsx
@@ -0,0 +1,46 @@
+import { createContext, PropsWithChildren, useEffect, useState } from 'react';
+
+import { getPlaces, GetPlacesRequestQuery} from 'src/api/places/places.api';
+
+import { Logger } from 'src/utils/logger';
+
+interface PlaceContextInterface {
+    places: Place[],
+    refreshPlaces: (query: GetPlacesRequestQuery) => Promise<void>;
+    isLoading: boolean;
+}
+
+export const PlacesContext = createContext<PlaceContextInterface>(null!);
+
+export const PlacesProvider = (props: PropsWithChildren<unknown>) => {
+    const [places, setPlaces] = useState<Place[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [abortController, setAbortController] = useState(new AbortController());
+
+    const refreshPlaces = async (query: GetPlacesRequestQuery) => {
+        try {
+            setIsLoading(true);
+
+            const recevicedPlaces = await getPlaces(query);
+
+            setPlaces(recevicedPlaces);
+        } catch(error) {
+            Logger.error('Cannot refresh places', error);
+        }
+        finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        return () => abortController.abort();
+    }, []);
+
+    return <PlacesContext.Provider value={{
+        places,
+        refreshPlaces,
+        isLoading
+    }}>
+        {props.children}
+    </PlacesContext.Provider>;
+};
---
+++ frontend/src/context/places/usePlaces.ts
@@ -0,0 +1,11 @@
+import { useContext } from 'react'
+import { PlacesContext } from './PlacesProvider'
+
+export const usePlaces = () => {
+    const context = useContext(PlacesContext);
+
+    if(!context)
+        throw new Error('usePlaces is used outside of its provider');
+
+    return context;
+}
