d081057b1d690a815b9c531aa6b116c643765b32
Waldemar Zieliński <waldemar99zielinski@gmail.com>
2023-01-15 18:29:58 +0100

    improvement(backend): set default username to id

--- backend/src/database/entities/user/user.sql.ts
+++ backend/src/database/entities/user/user.sql.ts
@@ -8,8 +8,8 @@ export const createUsersTable = `
 
         email               TEXT NOT NULL,
         is_email_confirmed  BOOLEAN NOT NULL DEFAULT FALSE,
-        username           TEXT NOT NULL DEFAULT uuid_generate_v4(),
-        
+        username            TEXT NOT NULL,
+
         password_hash       TEXT DEFAULT NULL,
         password_salt       TEXT DEFAULT NULL,
         auth_strategy       user_authstrategy_enum NOT NULL,
@@ -18,4 +18,21 @@ export const createUsersTable = `
         CONSTRAINT user_unique_email_auth_strategy_constraint UNIQUE(email, auth_strategy),
         UNIQUE(username)
     );
+
+    CREATE OR REPLACE FUNCTION default_username_as_id()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql AS
+    $$
+    BEGIN
+       NEW.username := NEW.id;
+   
+       RETURN NEW;
+    END;
+    $$;
+
+    CREATE TRIGGER default_username_trigger
+    BEFORE INSERT
+    ON "users"
+    FOR EACH ROW
+    EXECUTE PROCEDURE default_username_as_id();
 `;
