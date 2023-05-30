6324329bec76ba097504c1b04372b48a0dc159ea
Waldemar Zieliński <waldemar99zielinski@gmail.com>
2023-05-31 00:06:11 +0200

    fix(front): align misleading js date operators

--- frontend/src/utils/date/formatDate.ts
+++ frontend/src/utils/date/formatDate.ts
@@ -1,3 +1,3 @@
 export const formatDate = (date: Date) => {
-    return `${date.getDay().toString().padStart(2, '0')}-${date.getMonth().toString().padStart(2, '0')}-${date.getFullYear().toString()}`
+    return `${date.getDate().toString().padStart(2, '0')}-${(date.getMonth()+1).toString().padStart(2, '0')}-${date.getFullYear().toString()}`
 }
